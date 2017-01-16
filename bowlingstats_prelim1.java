59bebd0242fda3a927da79d0a9d5d8d0b638a933
roguerammer999 <timheverett@gmail.com>
2017-01-16 18:26:26 -0500

    Update bowlingstats_prelim1.java
    Main changes:
    1. I changed the class name and intend to follow my convention of starting with "bowlingstats".  The old class name was tongue-in-cheek, as I tend to do when not posting code.
    2. Several variables are changed slightly to traditional "camelCase" capitalization formatting.
    3. Because the number of balls in a bowling game can vary, I now use method "ballsplitter" to set the length of the "balls" array that tracks each ball, instead of setting its length at the maximimum of 21.
    4. I added some comments.  Up to this point, most of my programs are one-time test or practice runs with little subsequent modification, so I do not normally comment on those.
    5. Method "framescorer()" is residual, from earlier experimentation, but it was unused.  Therefore, I discarded it.

--- bowlingstats_prelim1.java
+++ bowlingstats_prelim1.java
@@ -1,123 +1,43 @@
-public class BK_superbowl_prelim1
+public class bowlingstats_prelim1
 {
     public static void main(String[] args)
     {
-        bgame1 g1_2016_12_30 =
-                new bgame1("4-6-7-1-7-1-1-7-9-1-8-0-7-0-0-10-10-9-0");
-        bgame1 g2_2016_12_30 =
-                new bgame1("9-1-8-0-8-2-7-0-10-8-2-10-9-0-10-0-10-9");
+        bgame_v1 g1_2016_12_30 =
+                new bgame_v1("4-6-7-1-7-1-1-7-9-1-8-0-7-0-0-10-10-9-0");
+        bgame_v1 g2_2016_12_30 =
+                new bgame_v1("9-1-8-0-8-2-7-0-10-8-2-10-9-0-10-0-10-9");
+        bgame_v1 g3_2016_12_30 = 
+                new bgame_v1("9-0-10-0-9-8-2-10-9-0-6-0-0-8-7-3-10-5");
+        bgame_v1 g4_2016_12_30 =
+                new bgame_v1("9-0-7-1-8-2-9-0-10-10-5-5-9-0-7-2-8-0");
     }
     
 }
 
-class bgame1
+//Class "bgame_v1" is meant to refer to an instance of a bowling game.  It
+//should contain a list of balls thrown (i.e. how many pins per ball)
+//as well as frame data (spares and strikes) and score data.
+//The "v1" is arbitrary.  I didn't want to call it "bgame" because I expect
+//to make future versions in the same directory.
+
+class bgame_v1
 {
     private String balls_str[];
-    private int balls[] = new int [21];
+    //Array "balls[]" is not created yet as its length will be determined
+    //in method "ballsplitter".
+    private int balls[];
+    
+    //"bg_frames" (bowling game frames) is an array of frame_v1 objects.
+    //Each frame contains a score for the first ball and (if the first was
+    //not a strike) second ball, as well as the score at the end of the frame.
     private frame_v1 bg_frames[] = new frame_v1 [12];
-    private int framecounter;
+    private int frameCounter;
     
-    
-    private void ballsplitter(String score_breakdown)
-    {
-        this.balls_str = score_breakdown.split("-");
-        for(int counter=0; counter<balls_str.length; counter++)
-        {
-            balls[counter] = Integer.parseInt(balls_str[counter]);
-        }
-    }
-    
-    private void framecreator()
-    {
-        framecounter = -1;
-        int lastframe;
-        lastframe = -1;
-        for(int ballthrow=0; ballthrow<21; ballthrow++)
-        {
-            if (ballthrow - lastframe == 1)
-            {
-                framecounter++;
-                bg_frames[framecounter] = new frame_v1();
-                if(framecounter>0)
-                {
-                    bg_frames[framecounter].score += bg_frames[framecounter-1].score;
-                }
-                if (balls[ballthrow] != 10)
-                {
-                    bg_frames[framecounter].ball1 = 
-                            Integer.toString(balls[ballthrow]);
-                }
-                else if (balls[ballthrow] == 10)
-                {
-                    bg_frames[framecounter].ball1 = " ";
-                    bg_frames[framecounter].ball2 = "X";
-                    bg_frames[framecounter].score +=
-                            balls[ballthrow+1];
-                    bg_frames[framecounter].score +=
-                            balls[ballthrow+2];
-                    lastframe = ballthrow;
-                }
-            }
-            else if (ballthrow - lastframe == 2)
-            {
-                lastframe = ballthrow;
-                if (balls[ballthrow] == 10-balls[ballthrow-1])
-                {
-                    bg_frames[framecounter].ball2 = "/";
-                    bg_frames[framecounter].score +=
-                            balls[ballthrow+1];
-                }
-                else if (balls[ballthrow] < 10-balls[ballthrow-1])
-                {
-                    bg_frames[framecounter].ball2 = 
-                            Integer.toString(balls[ballthrow]);
-                }
-            }
-            
-            bg_frames[framecounter].score += balls[ballthrow];
-        }
-    }
-    
-    private void framescorer()
-    {
-        for (int framect = 1; framect < 10; framect++)
-        {
-            bg_frames[framect].score += bg_frames[framect-1].score;
-            if(bg_frames[framect].ball2.equals("/"))
-                if(bg_frames[framect+1].ball2.equals("X"))
-                    bg_frames[framect].score+=10;
-                else if(!bg_frames[framect+1].ball2.equals("X"))
-                    bg_frames[framect].score+=0;
-                bg_frames[framect].score++;
-        }
-    }
-    
-    private void test_print()
-    {
-        for(int counter = 0; counter<12; counter++)
-        {
-            if(bg_frames[counter] != null)
-            {
-                System.out.print(bg_frames[counter].ball1 + " " +
-                            bg_frames[counter].ball2 + "  |  ");
-            }
-            else
-                ;
-        }
-        System.out.println("");
-        for(int counter = 0; counter<12; counter++)
-        {
-            if(bg_frames[counter] != null)
-            {
-                System.out.print("   " + bg_frames[counter].score + "  ");
-            }
-            else
-                ;
-        }
-        System.out.println("\n\n");
-    }
-    
-    public bgame1(String score_breakdown)
+    //This is the only valid constructor.  It accepts a string that
+    //consists of each ball (e.g. 9 pins the first ball, 1 pin the next)
+    //separated with a dash as a delimiter.  It immediately calls method
+    //"ballsplitter", which takes the score string as a parameter.
+    public bgame_v1(String score_breakdown)
     {
         ballsplitter(score_breakdown);
         framecreator();
@@ -128,5 +48,108 @@ class bgame1
     {
         String ball1, ball2;
         int score = 0;
+    }    
+    
+    //The method "ballsplitter" first splits the String parameter listing the
+    //game by its ball scores into an array String, then parses the array
+    //String into an array of integers.
+    
+    private void ballsplitter (String score_breakdown)
+    {
+        this.balls_str = score_breakdown.split("-");
+        this.balls = new int[balls_str.length];
+        for(int counter=0; counter<balls_str.length; counter++)
+        {
+            balls[counter] = Integer.parseInt(balls_str[counter]);
+        }
+    }
+    
+    private void framecreator()
+    {
+        frameCounter = -1;
+        //"prevFrame" is the most recent ball on which the frame was changed.
+        int prevFrame = -1;
+        for(int ballthrow=0; ballthrow<balls_str.length; ballthrow++)
+        {
+            if (ballthrow - prevFrame == 1)
+            {
+                frameCounter++;
+                bg_frames[frameCounter] = new frame_v1();
+                if(frameCounter>0)
+                {
+                    bg_frames[frameCounter].score += bg_frames[frameCounter-1].score;
+                }
+                if (balls[ballthrow] != 10)
+                {
+                    bg_frames[frameCounter].ball1 = 
+                            Integer.toString(balls[ballthrow]);
+                }
+                else if (balls[ballthrow] == 10)
+                {
+                    bg_frames[frameCounter].ball1 = " ";
+                    bg_frames[frameCounter].ball2 = "X";
+                    if(frameCounter<9)
+                    {
+                    bg_frames[frameCounter].score +=
+                            balls[ballthrow+1];
+                    bg_frames[frameCounter].score +=
+                            balls[ballthrow+2];
+                    }
+                    else;
+                    prevFrame = ballthrow;
+                }
+            }
+            else if (ballthrow - prevFrame == 2)
+            {
+                prevFrame = ballthrow;
+                if (balls[ballthrow] == 10-balls[ballthrow-1])
+                {
+                    bg_frames[frameCounter].ball2 = "/";
+                    if(frameCounter<9)
+                    bg_frames[frameCounter].score +=
+                            balls[ballthrow+1];
+                    else;
+                }
+                else if (balls[ballthrow] < 10-balls[ballthrow-1])
+                {
+                    bg_frames[frameCounter].ball2 = 
+                            Integer.toString(balls[ballthrow]);
+                }
+            }
+            
+            bg_frames[frameCounter].score += balls[ballthrow];
+        }
+    }
+    
+    private void test_print()
+    {
+        for(int counter = 0; counter<12; counter++)
+        {
+            if(bg_frames[counter] != null)
+            {
+                System.out.print(" " + bg_frames[counter].ball1 + " " +
+                            bg_frames[counter].ball2 + "  |  ");
+            }
+            else
+                ;
+        }
+        System.out.println("");
+        for(int counter = 0; counter<12; counter++)
+        {
+            String spacedel;
+            if(bg_frames[counter] != null)
+            {
+                if(bg_frames[counter].score < 10)
+                    spacedel = "    ";
+                else if(bg_frames[counter].score < 100)
+                    spacedel = "   ";
+                else
+                    spacedel = "  ";
+                System.out.print(spacedel + bg_frames[counter].score + " |  ");
+            }
+            else
+                ;
+        }
+        System.out.println("\n\n");
     }
 }
